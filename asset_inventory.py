de0d7ef547f93ce31d302ce6c7ead68242501ad3
sebastin-cr7 <2528050@saec.ac.in>
2026-09-18 06:38:10 +0530

    Add sample asset to asset inventory
    Added a sample asset dictionary and appended it to the assets list.

--- asset_inventory.py
+++ asset_inventory.py
@@ -1,1 +1,15 @@
 assets = []
+assets = []
+
+asset = {
+    "id": "A101",
+    "name": "HR-PC-01",
+    "type": "Workstation",
+    "ip": "192.168.1.10",
+    "os": "Windows 11",
+    "department": "HR",
+    "risk": "Medium",
+    "status": "Secure"
+}
+
+assets.append(asset)
